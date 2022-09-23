9efa66ad9c6ba35ff4cfc1e3c31cb408f25a4ca6
JulioOrtegaC <julio_papo@hotmail.com>
2022-09-22 19:27:34 -0500

    Cambios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require ('express')
-const bodyParse = require ('body-parse')
+const bodyParser = require ('body-parse')
 const server = express();
 const port = 8081
+server.use(bodyParser.json()); 
+server.use(bodyParser.urlencoded({ extended: false }));
 
 server.listen(port,()=>{ 
     console.log("Servidor corriendo en puerto 8081")
